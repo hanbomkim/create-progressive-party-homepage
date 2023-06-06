c10ade5acccf8e6d9b311b763d37aefa518c421d
hanbomzz <gksqha1@nate.com>
2023-06-06 19:50:22 +0900

    commit

--- src/components/Fullpage.tsx
+++ src/components/Fullpage.tsx
@@ -34,7 +34,7 @@ const Fullpage = () => {
               <ContentText>
                 언제나 (가칭)국민주권당은 국민 곁에 있습니다.
               </ContentText>
-              <RoutingButton href={"/project"}>
+              <RoutingButton href={"/invite-proposar"}>
                 (가칭)국민주권당 창당 발기인 참여하기
               </RoutingButton>
             </LandingSlideWrapper>
