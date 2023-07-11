90dcdcaff502b45b313d373bf913b7843a7fd506
hanbomzz <gksqha1@nate.com>
2023-07-11 23:57:35 +0900

    fix: 재배포 및 리다이렉트 설정

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -1,8 +1,8 @@
 const metadata = {
   meta: {
     url: "www.jugwon.kr",
-    title: "(가칭)국민주권당 추진위원회",
-    description: "(가칭)국민주권당의 발기인이 되어주세요.",
+    title: "(가칭)국민주권당 준비위원회",
+    description: "(가칭)국민주권당의 당원이 되어주세요.",
   },
 };
 
